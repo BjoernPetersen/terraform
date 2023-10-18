450fdf7c8cde194f9b1b98280bde0dcfec5a2ce2
Björn Petersen <git@bjoernpetersen.net>
2023-10-18 19:48:14 +0200

    Add mixology repos

---
+++ repogroup_mixology.tf
@@ -0,0 +1,26 @@
+module "mixology_api_repo" {
+  source       = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name         = "mixology-api"
+  description  = "Remixes your Spotify playlists"
+  homepage_url = "https://mix-api.bembel.party/"
+  required_status_checks = [
+    "check-format",
+    "lint",
+    "build-app-image",
+    "WIP",
+  ]
+}
+
+module "mixology_frontend_repo" {
+  source       = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name         = "mixology-frontend"
+  description  = "The Mixology frontend"
+  homepage_url = "https://mix.bembel.party"
+  required_status_checks = [
+    "Check Format",
+    "Lint",
+    "Build Web Package",
+    "Build Container Package",
+    "WIP",
+  ]
+}

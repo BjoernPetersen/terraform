3609e64190e972ff7cea0f0624c1995e95e9c365
Björn Petersen <git@bjoernpetersen.net>
2023-10-28 12:15:30 +0200

    Add legacy MusicBot repos

---
+++ repogroup_musicbot_legacy.tf
@@ -0,0 +1,52 @@
+module "musicbot_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "MusicBot"
+  description            = "Party music queue with songs from various providers"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}
+
+module "musicbot_api_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "MusicBot-API"
+  description            = "OpenAPI specification of the MusicBot REST-API"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}
+
+module "musicbot_bundler_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "MusicBot-bundler"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}
+
+module "musicbot_desktop_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "MusicBot-desktop"
+  description            = "JavaFX-based implementation of MusicBot for desktop systems"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}
+
+module "musicbot_plugins_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "MusicBot-plugins"
+  description            = "Collection of official MusicBot plugins"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}
+
+module "musicbot_qbert_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v4.0.1"
+  name                   = "Qbert"
+  description            = "Android implementation of MusicBot"
+  required_status_checks = []
+  enable_actions = false
+  is_archived = false
+}

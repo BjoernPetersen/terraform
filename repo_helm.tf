a76fc2290ef0357d99643429557870f2587967f3
Björn Petersen <git@bjoernpetersen.net>
2023-11-12 00:50:48 +0100

    Rename helm repository

--- repo_helm.tf
+++ repo_helm.tf
@@ -1,6 +1,6 @@
 module "helm_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
-  name        = "helm"
+  name        = "container-helm"
   description = "Container image based on Debian with kubectl and helm installed"
   is_public   = false
   required_status_checks = [

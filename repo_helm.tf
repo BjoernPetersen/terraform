d97263dcd3d468d9dd0ed8316013af6253812554
Björn Petersen <git@bjoernpetersen.net>
2023-11-12 00:34:58 +0100

    Add helm repo

---
+++ repo_helm.tf
@@ -0,0 +1,11 @@
+module "helm_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name        = "helm"
+  description = "Container image based on Debian with kubectl and helm installed"
+  is_public  = false
+  required_status_checks = [
+    "check / check",
+    "build-container-image (amd64) / build",
+    "build-container-image (arm64) / build",
+  ]
+}

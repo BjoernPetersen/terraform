d20982dd74d4aa02c8973f4b369169fc389e267d
Björn Petersen <git@bjoernpetersen.net>
2023-12-02 14:22:24 +0100

    aoc: Add status checks

--- repogroup_adventofcode.tf
+++ repogroup_adventofcode.tf
@@ -1,41 +1,52 @@
 module "adventofcode_repo" {
   for_each = {
     "2018" = {
-      is_archived = true,
-      is_public   = false,
-      language    = "Dart"
+      is_archived   = true,
+      is_public     = false,
+      language      = "Dart",
+      status_checks = [],
     },
     "2019" = {
-      is_archived = true,
-      is_public   = true,
-      language    = "Dart"
+      is_archived   = true,
+      is_public     = true,
+      language      = "Dart",
+      status_checks = [],
     },
     "2020" = {
-      is_archived = true,
-      is_public   = true,
-      language    = "Rust"
+      is_archived   = true,
+      is_public     = true,
+      language      = "Rust",
+      status_checks = [],
     },
     "2021" = {
-      is_archived = true,
-      is_public   = false,
-      language    = "Python"
+      is_archived   = true,
+      is_public     = false,
+      language      = "Python",
+      status_checks = [],
     },
     "2022" = {
-      is_archived = true,
-      is_public   = true,
-      language    = "Python"
+      is_archived   = true,
+      is_public     = true,
+      language      = "Python",
+      status_checks = [],
     },
     "2023" = {
-      is_archived = true,
-      is_public   = true,
-      language    = "Dart"
+      is_archived   = true,
+      is_public     = true,
+      language      = "Dart",
+      status_checks = [
+        "validate-renovate-config",
+        "test",
+        "check-format",
+        "lint",
+      ],
     },
   }
 
   source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
   name                   = "advent-of-code-${each.key}"
   description            = "Advent of Code ${each.key} solutions written in ${each.value.language}"
-  required_status_checks = []
+  required_status_checks = each.value.status_checks
   is_archived            = each.value.is_archived
   is_public              = each.value.is_public
 }

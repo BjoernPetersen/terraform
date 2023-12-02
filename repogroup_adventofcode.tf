e15d260baaaac34cc63a68b09bf8d7c2dc5bc1ba
Björn Petersen <git@bjoernpetersen.net>
2023-12-02 14:00:36 +0100

    Add advent-of-code repos

---
+++ repogroup_adventofcode.tf
@@ -0,0 +1,36 @@
+module "adventofcode_repo" {
+  for_each = {
+    "2018" = {
+      is_archived = false,
+      is_public   = false,
+      language    = "Dart"
+    },
+    "2019" = {
+      is_archived = false,
+      is_public   = true,
+      language    = "Dart"
+    },
+    "2020" = {
+      is_archived = false,
+      is_public   = true,
+      language    = "Rust"
+    },
+    "2021" = {
+      is_archived = false,
+      is_public   = false,
+      language    = "Python"
+    },
+    "2022" = {
+      is_archived = true,
+      is_public   = true,
+      language    = "Python"
+    },
+  }
+
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name                   = "advent-of-code-${each.key}"
+  description            = "Advent of Code ${each.key} solutions written in ${each.value.language}"
+  required_status_checks = []
+  is_archived            = each.value.is_archived
+  is_public              = each.value.is_public
+}

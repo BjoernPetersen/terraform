a7810e10a57dcbd6325a1e2e4fee96bfadec9c66
Björn Petersen <git@bjoernpetersen.net>
2024-04-04 19:16:13 +0200

    Add k8s repos

---
+++ repogroup_k8s.tf
@@ -0,0 +1,22 @@
+module "k8s_repos" {
+  for_each = {
+    "k8s-certmanager" : {
+      description = "Cert-Manager installation for home k8s cluster",
+    },
+    "k8s-gateway" : {
+      description = "Gateway ingresses on home k8s cluster",
+    },
+    "k8s-github-runner" : {
+      description = "Self-hosted GitHub Runner Controller in home k8s cluster",
+    },
+    "k8s-namespaces" : {
+      description = "Namespaces and permissions as code for home k8s cluster",
+    },
+  }
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.0.0"
+  name                   = each.key
+  description            = each.value.description
+  required_status_checks = [
+    "lint",
+  ]
+}

de22820b46ac1289063354b3b12c1fb08cde3dca
Björn Petersen <git@bjoernpetersen.net>
2024-04-05 22:53:13 +0200

    Add argocd rules for dyndns

--- repo_cloudflare_dyndns.tf
+++ repo_cloudflare_dyndns.tf
@@ -1,7 +1,8 @@
 module "cloudflare_dyndns_repo" {
-  source       = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.0.0"
-  name         = "cloudflare-dyndns"
-  description  = "FRITZ!Box-compatible DynDNS service for a Cloudflare-managed DNS zone"
-  is_public    = false
-  homepage_url = "https://dyndns.bjoernpetersen.net"
+  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.0"
+  name                = "cloudflare-dyndns"
+  description         = "FRITZ!Box-compatible DynDNS service for a Cloudflare-managed DNS zone"
+  is_public           = false
+  homepage_url        = "https://dyndns.bjoernpetersen.net"
+  enable_argocd_rules = true
 }

cd247d9ade462e48845488bb0e85f518cd5ecb4c
Björn Petersen <git@bjoernpetersen.net>
2024-04-24 20:28:42 +0200

    Add hetzner repo

---
+++ repo_hetzner.tf
@@ -0,0 +1,35 @@
+module "hetzner_repo" {
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.1"
+  name   = "hetzner"
+  required_status_checks = [
+    "fmt",
+    "validate",
+    "plan",
+  ]
+  description    = "Hetzner Cloud resources"
+  is_public = false
+}
+
+resource "google_service_account" "hetzner" {
+  account_id   = "github-actions-ci-hetzner"
+  display_name = "GitHub Actions Hetzner repo"
+}
+
+resource "google_project_iam_member" "hetzner_roles" {
+  for_each = toset([
+    "roles/storage.objectAdmin",
+  ])
+  project = google_service_account.hetzner.project
+  role    = each.key
+  member  = google_service_account.hetzner.member
+}
+
+resource "google_service_account_key" "hetzner" {
+  service_account_id = google_service_account.hetzner.account_id
+}
+
+resource "github_actions_secret" "hetzner_gsa" {
+  repository      = module.hetzner_repo.name
+  secret_name     = "GOOGLE_SA_JSON"
+  plaintext_value = base64decode(google_service_account_key.hetzner.private_key)
+}

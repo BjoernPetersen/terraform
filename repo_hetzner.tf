d34dfa4771b20a5b5e9bd2bd2702b591b8da42ed
Björn Petersen <git@bjoernpetersen.net>
2024-04-24 20:33:18 +0200

    Move hetzner repo

--- repo_hetzner.tf
+++ repo_hetzner.tf
@@ -1,35 +1,35 @@
-module "hetzner_repo" {
+module "hetzner_plex_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.1"
-  name   = "hetzner"
+  name   = "hetzner-plex"
   required_status_checks = [
     "fmt",
     "validate",
     "plan",
   ]
-  description = "Hetzner Cloud resources"
+  description = "Hetzner Cloud Plex project"
   is_public   = false
 }
 
-resource "google_service_account" "hetzner" {
+resource "google_service_account" "hetzner_plex" {
   account_id   = "github-actions-ci-hetzner"
   display_name = "GitHub Actions Hetzner repo"
 }
 
-resource "google_project_iam_member" "hetzner_roles" {
+resource "google_project_iam_member" "hetzner_plex_roles" {
   for_each = toset([
     "roles/storage.objectAdmin",
   ])
-  project = google_service_account.hetzner.project
+  project = google_service_account.hetzner_plex.project
   role    = each.key
-  member  = google_service_account.hetzner.member
+  member  = google_service_account.hetzner_plex.member
 }
 
 resource "google_service_account_key" "hetzner" {
-  service_account_id = google_service_account.hetzner.account_id
+  service_account_id = google_service_account.hetzner_plex.account_id
 }
 
 resource "github_actions_secret" "hetzner_gsa" {
-  repository      = module.hetzner_repo.name
+  repository      = module.hetzner_plex_repo.name
   secret_name     = "GOOGLE_SA_JSON"
   plaintext_value = base64decode(google_service_account_key.hetzner.private_key)
 }

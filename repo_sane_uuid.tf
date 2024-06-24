992cd3784302c42619bdbf18a799568255ddecff
Björn Petersen <git@bjoernpetersen.net>
2024-06-24 20:27:31 +0200

    sane_uuid: Update description

--- repo_sane_uuid.tf
+++ repo_sane_uuid.tf
@@ -1,7 +1,7 @@
 module "sane_uuid_repo" {
   source             = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.1.1"
   name               = "sane_uuid"
-  description        = "UUID generation and parsing with a sane interface"
+  description        = "UUID generation and parsing with a reasonable interface"
   homepage_url       = "https://pub.dev/packages/sane_uuid"
   enable_discussions = true
   required_status_checks = [

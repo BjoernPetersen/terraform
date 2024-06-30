dfa822c25cec83cff0cd27f1557337f50446e990
Björn Petersen <git@bjoernpetersen.net>
2024-06-30 09:29:52 +0200

    Add actions-container

---
+++ repo_actions_container.tf
@@ -0,0 +1,8 @@
+module "actions_container_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                   = "actions-container"
+  description            = "Mirrors the tags of the BlindfoldedSurgery repo with the same name"
+  is_public              = true
+  enable_argocd_rules    = false
+  required_status_checks = []
+}

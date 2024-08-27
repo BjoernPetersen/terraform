9e66a98697bae0087e0532551d0ef9e1fed8445b
Björn Petersen <git@bjoernpetersen.net>
2024-08-27 10:13:11 +0200

    Add ShutdownServer

---
+++ repo_shutdown_server.tf
@@ -0,0 +1,10 @@
+module "shutdown_server_repo" {
+  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                   = "ShutdownServer"
+  description            = "HTTP server to remotely trigger shutdowns (and more)"
+  required_status_checks = []
+  default_branch_name    = "master"
+  blocked_branches       = []
+  enable_argocd_rules    = false
+}
+

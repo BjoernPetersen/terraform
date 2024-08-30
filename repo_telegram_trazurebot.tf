59c70f034b887470c34959552f22d60c546417b0
Björn Petersen <git@bjoernpetersen.net>
2024-08-30 12:13:00 +0200

    Add trazurebot

---
+++ repo_telegram_trazurebot.tf
@@ -0,0 +1,12 @@
+module "transbot_repo" {
+  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
+  name                = "telegram-trazurebot"
+  description         = "Voice message transcription for Telegram, based on Azure Cognitive Services"
+  is_public           = false
+  enable_argocd_rules = true
+}
+
+import {
+  to = module.transbot_repo.github_repository.main
+  id = "telegram-trazurebot"
+}

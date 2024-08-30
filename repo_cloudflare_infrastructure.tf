19d361e45c6752eccc82e7a2446d21df9820db72
Björn Petersen <git@bjoernpetersen.net>
2024-08-30 17:20:36 +0200

    Use module for cloudflare-infrastructure GSA

--- repo_cloudflare_infrastructure.tf
+++ repo_cloudflare_infrastructure.tf
@@ -10,26 +10,14 @@ module "cloudflare_infrastructure_repo" {
   is_public = false
 }
 
-resource "google_service_account" "cloudflare_infrastructure" {
-  account_id   = "gha-cloudflare-infrastructure"
-  display_name = "GitHub Actions Hetzner repo"
-}
-
-resource "google_project_iam_member" "cloudflare_infrastructure_roles" {
-  for_each = toset([
-    "roles/storage.objectAdmin",
-  ])
-  project = google_service_account.cloudflare_infrastructure.project
-  role    = each.key
-  member  = google_service_account.cloudflare_infrastructure.member
-}
-
-resource "google_service_account_key" "cloudflare_infrastructure" {
-  service_account_id = google_service_account.cloudflare_infrastructure.account_id
+module "cloudflare_infrastructure_terraform" {
+  source       = "./modules/terraform_state_access"
+  display_name = "GitHub Actions self/cloudflare-infrastructure"
+  identifier   = "cloudflare-infrastructure"
 }
 
 resource "github_actions_secret" "cloudflare_infrastructure_gsa" {
   repository      = module.cloudflare_infrastructure_repo.name
   secret_name     = "GOOGLE_SA_JSON"
-  plaintext_value = base64decode(google_service_account_key.cloudflare_infrastructure.private_key)
+  plaintext_value = module.cloudflare_infrastructure_terraform.private_key
 }

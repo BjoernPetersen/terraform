1b9419d9806905e9c24470f20a9a900dd802e8e2
Björn Petersen <git@bjoernpetersen.net>
2024-08-30 15:55:16 +0200

    Make trazurebot public

--- repo_telegram_trazurebot.tf
+++ repo_telegram_trazurebot.tf
@@ -2,6 +2,6 @@ module "transbot_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.2.0"
   name                = "telegram-trazurebot"
   description         = "Voice message transcription for Telegram, based on Azure Cognitive Services"
-  is_public           = false
+  is_public           = true
   enable_argocd_rules = true
 }

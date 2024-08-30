5d2447ec1866295a2d5c728383bdd06b588c4476
Björn Petersen <git@bjoernpetersen.net>
2024-08-30 16:09:41 +0200

    Add Google setup for azure-infrastructure

--- repo_azure_infrastructure.tf
+++ repo_azure_infrastructure.tf
@@ -9,3 +9,27 @@ module "azure_infrastructure_repo" {
   ]
   is_public = false
 }
+
+resource "google_service_account" "azure_infrastructure" {
+  account_id   = "gha-azure-infrastructure"
+  display_name = "GitHub Actions Hetzner repo"
+}
+
+resource "google_project_iam_member" "azure_infrastructure_roles" {
+  for_each = toset([
+    "roles/storage.objectAdmin",
+  ])
+  project = google_service_account.azure_infrastructure.project
+  role    = each.key
+  member  = google_service_account.azure_infrastructure.member
+}
+
+resource "google_service_account_key" "azure_infrastructure" {
+  service_account_id = google_service_account.azure_infrastructure.account_id
+}
+
+resource "github_actions_secret" "azure_infrastructure_gsa" {
+  repository      = module.azure_infrastructure_repo.name
+  secret_name     = "GOOGLE_SA_JSON"
+  plaintext_value = base64decode(google_service_account_key.azure_infrastructure.private_key)
+}

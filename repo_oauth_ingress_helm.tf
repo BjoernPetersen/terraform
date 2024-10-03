a0ecbdbf9063871926698a1035268602b97ebf8a
Björn Petersen <git@bjoernpetersen.net>
2024-10-03 17:13:57 +0200

    Add new repo

---
+++ repo_oauth_ingress_helm.tf
@@ -0,0 +1,14 @@
+module "oauth_ingress_helm_repo" {
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v7.3.2"
+  name   = "oauth-ingress-helm"
+
+  is_public           = false
+  enable_argocd_rules = false
+
+  required_status_checks = []
+}
+
+import {
+  id = "oauth-ingress-helm"
+  to = module.oauth_ingress_helm_repo.github_repository.main
+}

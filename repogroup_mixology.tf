5a66638c804dc139dfca525d342bc4809c295fbf
Björn Petersen <git@bjoernpetersen.net>
2024-12-12 20:09:43 +0100

    Prepare mixology for archival

--- repogroup_mixology.tf
+++ repogroup_mixology.tf
@@ -1,26 +1,25 @@
 module "mixology_api_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.1"
-  name                = "mixology-api"
-  description         = "Remixes your Spotify playlists"
-  homepage_url        = "https://mix-api.bembel.party/"
-  enable_argocd_rules = true
-  required_status_checks = [
-    "check-format",
-    "lint",
-    "build-app-image / build",
-    "build-migrations-image / build",
-  ]
+  source       = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.1"
+  name         = "mixology-api"
+  description  = "Remixes your Spotify playlists"
+  homepage_url = "https://mix-api.bembel.party/"
+
+  enable_argocd_rules          = false
+  include_required_meta_checks = false
+  enable_actions               = false
+  required_status_checks       = []
+  blocked_branches             = []
 }
 
 module "mixology_frontend_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.1"
-  name                = "mixology-frontend"
-  description         = "The Mixology frontend"
-  homepage_url        = "https://mix.bembel.party"
-  enable_argocd_rules = true
-  required_status_checks = [
-    "Check Format",
-    "Lint",
-    "build-container-image / build",
-  ]
+  source       = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.1"
+  name         = "mixology-frontend"
+  description  = "The Mixology frontend"
+  homepage_url = "https://mix.bembel.party"
+
+  enable_argocd_rules          = false
+  include_required_meta_checks = false
+  enable_actions               = false
+  required_status_checks       = []
+  blocked_branches             = []
 }

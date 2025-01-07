c5c51191fc6ed9fcb8816541ba58ac5fea727a91
Björn Petersen <git@bjoernpetersen.net>
2025-01-07 23:38:23 +0100

    Remove trazurebot

--- repo_telegram_trazurebot.tf
+++
@@ -1,8 +0,0 @@
-module "transbot_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v8.0.3"
-  name                = "telegram-trazurebot"
-  description         = "Voice message transcription for Telegram, based on Azure Cognitive Services"
-  is_public           = true
-  enable_argocd_rules = true
-  is_archived         = true
-}
